606e4c0114518657ab0f28b26ee36b71b4ffaff6
j in paradise <public.park.ji@gmail.com>
2022-04-02 23:53:25 +0900

    ui

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GoogleAuth from './GoogleAuth';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="SmallContainer">
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,12 @@
+
+const GoogleAuth = () => {
+  return (
+    <div>
+      <div>
+        <button>구글로긴</button>
+      </div>
+    </div>
+  )
+}
+
+export default GoogleAuth;
