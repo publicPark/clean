a4fb9cd201e32b60584b683f1a70c4e93efba9cb
Jiyun Park <public.park.ji@gmail.com>
2022-06-29 17:40:37 +0900

    doc

--- src/data/texts.js
+++ src/data/texts.js
@@ -1,6 +1,11 @@
 export const texts = {
   title: {
-    ko: "즐청",
-    en: "Clean Roommate",
-  }
+    ko: "클룸메",
+    en: "Cloommate",
+  },
+  description: {
+    ko: "클룸메 - 청소해요! 룸메이트, 심판의 날이 다가와요..",
+    en: "Cloommate: Clean up! My roommate, The judgement day is waiting you..",
+  },
+
 }
